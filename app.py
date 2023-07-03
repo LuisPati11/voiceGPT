93c0a51704f9b28c6e558dda25e16493c2c36587
Pati <l.patino.c11@gmail.com>
2023-07-03 14:12:29 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import speech_recognition as sr
+import openai
+
+recording = sr.Recognizer()
+
+with sr.Microphone() as source: 
+    recording.adjust_for_ambient_noise(source)
+    print("Please Say something:")
+    audio = recording.listen(source)
+    answer = recording.recognize_google(audio)
+    try:
+        print("You said: " + answer)
+    except Exception as e:
+        print(e)
+
+openai.api_key = "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"
+
+completion = openai.Completion.create(engine="text-davinci-003",
+                         prompt=answer,
+                         max_tokens="2048")
+
+print(completion.choices[0].text)
